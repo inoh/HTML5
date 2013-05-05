58b27c34ad37b815cd56e1b904280329162c50d5
Hiroyuki Inoue <syslink.h.inoue@gmail.com>
2013-05-06 06:54:43 +0900

    running pusher sample

---
+++ websocket/pusher/config/initializers/pusher.rb
@@ -0,0 +1,8 @@
+# Pusher.app_id = 'your-pusher-app-id'
+# Pusher.key = 'your-pusher-key'
+# Pusher.secret = 'your-pusher-secret'
+Pusher.app_id = '43117'
+Pusher.key = 'c0adf081cd0f84f6bec8'
+Pusher.secret = '8ebcece14a14e7a7b4dd'
+
+Pusher.logger = Rails.logger
